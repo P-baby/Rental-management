2c30b6ec0a839c93bea787abba92bb860d75055c
p-baby <preciousepochi30@gmail.com>
2026-04-10 08:29:28 +0100

    tired

--- user/rentalhistory.php
+++ user/rentalhistory.php
@@ -28,6 +28,7 @@ $stmt->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rental History</title>
+    <link rel="stylesheet" href="../css/userdashboard.css">
 </head>
 <body>
     <div class="userdashboard_content" id="content_area">
--- user/user_search.php
+++ user/user_search.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>search and rent</title>
+    <link rel="stylesheet" href="../css/userdashboard.css">
 </head>
 <body>
 
